a8a1c88f059cede2112ef52c5f11dfd491d6f114
Chanyeong_Lee <zizonxm@naver.com>
2023-11-07 19:19:24 +0900

    new

---
+++ 08.OOP_p/Architecture.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.OOP_p
+{
+    internal class Architecture
+    {
+        // 추상클래스와 인터페이스
+        // 인터페이스는 추상클래스의 일종으로 특징이 동일함
+        // 공통점 : 함수에 대한 선언만 정의하고 이를 포함하는 클래스에서 구체화하여 사용
+        // 차이점 : 추상클래스는 다중 상속 허용 X, 인터페이스는 여러포함 허용O
+
+        // 개발자는 인터페이스와 추상클래스 중 더욱 상황에 적합한 것으로 구현해야함
+        // 추상클래스 (A is B 관계) : 상속 관계인 경우, 자식클래스가 부모클래스의 하위분류인 경우
+        // 인터페이스 (A can B 관계) : 행동 포함인 경우, 클래스가 해당 행동을 할 수 있는 경우
+
+        // 비유 설명으로 추상클래스는 설계도, 인터페이스는 계약서
+
+        public interface IEnterable
+        {
+            void Enter();
+
+            void Exit();
+        }
+
+        public interface IWindow
+        {
+            void OpenWindow();
+            void CloswWindow();
+        }
+
+        public interface ILightable
+        {
+            void TurnOffLight();
+            void TurnOnLight();
+        }
+        public abstract class Building : IEnterable, IWindow, ILightable
+        {
+
+            public void Enter()
+            {
+                Console.WriteLine("건물에 들어갑니다.")
+            }
+            public void Exit()
+            {
+                Console.WriteLine("건물에서 나갑니다.");
+            }
+
+            public abstract void OpenWindow();
+            public abstract void CloseWindow();
+
+            public abstract void TurnOffLight();
+            public abstract void TurnOnLight(); 
+        }
+
+        public class Bank : Building
+        {
+            public override void OpenWindow() { }
+            public override void CloseWindow() { }
+
+            public override void TurnOffLight()
+            {
+            }
+            public override void TurnOnLight()
+            {
+            }
+        }
+
+        public class Train : IEnterable, ILightable, IWindow
+        {
+            public void Enter() { } 
+            public void Exit() { }
+
+            public void TurnOffLight()
+            {
+
+            }
+
+            public void TurnOnLight()
+            { }
+
+            public void OpenWindow()
+            {
+
+            }
+
+            public void CloseWindow()
+            {
+
+            }
+        }
+    }
+}
---
+++ 10.Interface_p/Interface_p.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Interface_p
+{
+    internal class Interface_p
+    {
+        /********************************
+         * 인터페이스 (Interface) 
+         * 
+         * 인터페이스는 멤버를 가질 수 있지만 직접 구현하지 않음, 단지 정의만을 가짐
+         * 인터페이스를 가지는 클래스에서 반드시 인터페이스의 정의를 구현해야함
+         * 이를 반대로 표현하자면 인터페이스를 포함하는 클래스는
+         * 반드시 인터페이스의 구성요소들을 구현했다는 것을 보장함
+         *********************************/
+        
+        // <인터페이스 정의>
+        // 일반적으로 인터페이스 이름은 I로 시작하도록 네이밍
+
+        public interface IDamagable
+        {
+            void TakeHit();     // 직접 구현하지 않고 정의만 진행
+        }
+
+        public interface IMovable
+        {
+            void TakeMove();
+        }
+        
+        // <인터페이스 포함>
+        // 상속처럼 정의한 인터페이스를 클래스 : 인터페이스이름 으로 선언 
+        public class Monster : IDamagable
+        {
+            // 반드시 인터페이스에서 정의한 요소를 구현해야 함, 하지 않을 경우 오류
+            public void TakeHit()
+            {
+                Console.WriteLine("몬스터가 공격을 받습니다.");
+            }
+        }
+
+        public class Box : IDamagable, IMovable
+        { 
+            public void TakeHit()
+            {
+                Console.WriteLine("박스가 공격을 받아 부셔집니다.");
+            }
+
+            public void TakeMove()
+            {
+                Console.WriteLine("박스가 이동당합니다.");
+            }
+        }
+
+        public class Key : IMovable
+        {
+            public void TakeMove()
+            {
+                Console.WriteLine("열쇠를 얻었습니다.");
+            }
+        }
+
+        public class Player
+        {
+            public void Attack(IDamagable damagable)
+            {
+                Console.WriteLine("플레이어가 공격합니다.");
+                damagable.TakeHit();
+            }
+
+            public void Grab(IMovable movable)
+            {
+                Console.WriteLine("플레이어가 물체를 잡습니다.");
+                movable.TakeMove();
+            }
+
+
+        }
+
+        public class InterfaceTest
+        {
+            public void Test1()
+            {
+                Player player = new Player();
+                Monster monster = new Monster();
+                Box box = new Box();
+                Key key = new Key();
+
+                player.Attack(monster);
+                player.Attack(box);
+                player.Grab(box);
+                player.Grab(key);
+            }
+        }
+    }
+}
